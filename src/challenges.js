2110b8a7dee84507fa917cba5348699ed6fc719e
gabrieldeori <deori.pessoal@gmail.com>
2020-09-02 11:32:40 -0300

    Arrumando o arquivo

--- src/challenges.js
+++ src/challenges.js
@@ -1,31 +1,64 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(bool1, bool2) {
+  let boolR = false;
+  if (bool1 && bool2) {
+    boolR = true;
+  }
+  return boolR;
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return ((base * height) / 2);
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(frase) {
+  let palavra = '';
+  let arrayPalavras = [];
+  for (let i in frase) {
+    if (frase[i] !== ' ') {
+      palavra += frase[i];
+    } else {
+      arrayPalavras.push(palavra);
+      palavra = '';
+    }
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  return `${array[array.length - 1]}, ${array[0]}`;
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+    return (3 * wins) + ties;
 }
 
 // Desafio 6
 function highestCount() {
-  // seu código aqui
+  let posMaisF = 0;
+  let somaMaisF = somaRepeat(array, posMaisF);
+  let posAtual = posMaisF + 1;
+  let somaAtual = 0;
+  for (posAtual; posAtual < array.length; posAtual += 1) {
+    somaAtual = somaRepeat(array, posAtual);
+    if (somaAtual > somaMaisF) {
+      posMaisF = posAtual;
+      somaMaisF = somaAtual;
+    }
+  }
+  return `Posição: ${posMaisF} Soma: ${somaMaisF}`;
+}
+// Desafio 6 - Auxiliar
+function somaRepeat(array, pos) {
+  let soma = 0;
+  for (let i in array) {
+    if (array[pos] === array[i]) {
+      soma += 1;
+    }
+  }
+  return soma;
 }
 
 // Desafio 7
