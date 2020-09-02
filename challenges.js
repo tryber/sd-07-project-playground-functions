bcf64b6bd9410d425e121ae47d54ee225c1dcd32
gabrieldeori <deori.pessoal@gmail.com>
2020-09-02 09:17:18 -0300

    Exercício-1 concluído

---
+++ challenges.js
@@ -0,0 +1,25 @@
+let a,b;
+
+/* -----------------
+|  Área de funções |
+-------------------- */
+// 1) Usando Operador &&
+function compareTrue(bool1, bool2){
+    if(bool1 && bool2){
+        return true;
+    } else{
+        return false;
+    }
+}
+// 2) Área do triângulo
+
+/* -----------------
+| Área de execução |
+-------------------- */
+
+// 1) Usando Operador &&
+a = true;
+b = true;
+console.log(compareTrue(a, b));
+
+// 2) Área do triângulo
