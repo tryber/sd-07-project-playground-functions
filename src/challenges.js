088822eed9a4f8c812910e9d0fc73e473f4a0277
lukeCruz <enio.cruz@yahoo.com.br>
2020-09-10 01:21:31 -0300

    refatoração

--- src/challenges.js
+++ src/challenges.js
@@ -1,36 +1,48 @@
 // Desafio 1
-
 function compareTrue(a, b) {
-  // seu código aqui
-let compare = a && b;
-return compare;
+  let compare = a && b;
+  return compare;
 }
-
-  //let a= false;
-//let b= false;
-  //if (a === true && b === true) {
-   // return true;
-  //} else {
-    //return false;
-  //};
-
-
-// Desafio2 - CORRETO
-
-function calcArea(base,height) {
-
-return (base*height)/2;
+console.log(compareTrue(true, true));
+// Desafio 2
+function calcArea(base, height) {
+  let trianguloArea = ((base * height)/2);
+  return trianguloArea;
 }
-
-
-// Desafio 3 -CORRETO
+console.log(calcArea(8, 4));
+// Desafio 3
 function splitSentence(frase) {
- let palavras;
- palavras = frase.split (" ");
- return palavras;
+  let aPalavras = frase.split(' ');
+  return aPalavras;
 }
-
-
+console.log(splitSentence('go trybe'));
+// Desafio 4
+function concatName(nomes) {
+  let tamanho = nomes[nomes.length -1] + `, ` + nomes[0];
+  return tamanho;
+}
+console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
+// Desafio 5
+function footballPoints(wins, ties ) {
+  pontos = (wins * 3) + ties;
+  return pontos;
+}
+console.log(footballPoints(7, 5));
+// Desafio 6
+function highestCount(numeros) {
+  let repete = 0;
+  let maiorNum = 0;
+  for (let i = 0; i < numeros.length; i += 1) {
+    if (numeros[i] > maiorNum) {
+      maiorNum = numeros[i];
+      repete = 1;
+    } else if (numeros[i] === maiorNum) {
+      repete += 1;
+    }
+  }
+  return repete;
+}
+console.log(highestCount([9, 1, 2, 3, 9, 5, 7]))
 
 module.exports = {
   calcArea,
